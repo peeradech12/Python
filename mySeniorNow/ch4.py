6cbf7fd48886d3b8be84f7f659adccb8b9b544cd
peeradech12 <51998280+peeradech12@users.noreply.github.com>
2023-08-27 14:33:28 +0700

    Delete mySeniorNow directory

--- mySeniorNow/ch4.py
+++
@@ -1,7 +0,0 @@
-import nltk
-#nltk.download('averaged_perceptron_tagger')
-sentence = 'On the 15th of September, Tim Cook announced that Apple wants to acquire ABC Group from New York for 1 billion dollars.'
-tokenized_sent = nltk.word_tokenize(sentence)
-tagged_sent = nltk.pos_tag(tokenized_sent)
-#print(tagged_sent)
-print(nltk.ne_chunk(tagged_sent))
--- mySeniorNow/wtnk.py
+++
@@ -1,4 +0,0 @@
-import nltk
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-word_tokenize("Hi there!")
